7be4b625cebdefc0aebda2efe289b4c85a7aa08e
Tyoma Kukharuk <kukharuk.artem@gmail.com>
2021-10-05 23:02:17 +0300

    Добавьте файлы проекта.

---
+++ Player.cs
@@ -0,0 +1,43 @@
+﻿using System;
+using System.Collections;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Game1
+{
+    public class Player
+    {
+
+        float hp, atk, dmg, def, spd, crtrate, crtdmg;
+        int inv, money;
+        string name;
+        ArrayList invertory;
+
+        public Player()
+        {
+            hp = 100;
+            atk = 1;
+            dmg = 10;
+            def = 1;
+            spd = 1;
+            crtrate = 30;
+            crtdmg = 50;
+            inv = 10;
+            money = 0;
+            name = "player";
+        }
+
+        int Damage()
+        {
+            Random rnd = new Random();
+            if (crtrate < rnd.Next(100))
+            {
+                return (int)((atk * 0.1 * dmg) * (1 + (0.01 * crtdmg)));
+            }
+            else return (int)(atk * 0.1 * dmg);
+
+        }
+    }
+}
---
+++ WindowSettings.xaml.cs
@@ -0,0 +1,39 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows;
+using System.Windows.Controls;
+using System.Windows.Data;
+using System.Windows.Documents;
+using System.Windows.Input;
+using System.Windows.Media;
+using System.Windows.Media.Imaging;
+using System.Windows.Shapes;
+
+namespace Game1
+{
+    /// <summary>
+    /// Логика взаимодействия для WindowSettings.xaml
+    /// </summary>
+    public partial class WindowSettings : Window
+    {
+
+        Window father;
+
+        public WindowSettings(Window window)
+        {
+            InitializeComponent();
+            father = window;
+            father.IsEnabled = false;
+            
+        }
+
+        private void SaveSettingsButton_Click(object sender, RoutedEventArgs e)
+        {
+            father.IsEnabled = true;
+            Close();
+        }
+    }
+}
